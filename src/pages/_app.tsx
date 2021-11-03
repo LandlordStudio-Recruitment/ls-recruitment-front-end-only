2c5183a5368778108e8dcaa58e10a6608c630a00
Smock <sam.mockridge@landlordstudio.com>
2021-11-03 16:12:19 +1300

    update comment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // This the app component of the NextJs web application.
-  // To complete the test, you don't need to edit this (it is just a single page), however you can use it to edit it to include things like global layout,
+  // To complete the test, you don't need to edit this (the test is just a single page), however you can use it to edit it to include things like global layout,
   // config, styles etc.
   // e.g. <Layout><Component {...pageProps} /></Layout>
   // If you are just looking to make changes to the root page of the webapp, look at ~/src/pages/index.tsx.
